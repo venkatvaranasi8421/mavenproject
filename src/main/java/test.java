e12f8e3692211e34885b11a68cc4e1ffc441842c
venkatvaranasi8421 <100311800+venkatvaranasi8421@users.noreply.github.com>
2022-02-23 12:22:05 -0800

    Create test.java

---
+++ src/main/java/test.java
@@ -0,0 +1,7 @@
+class test{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+        System.out.println("Hello Java123");  
+         System.out.println("Hello Java45dfdgfdfg");  
+    }  
+}  
